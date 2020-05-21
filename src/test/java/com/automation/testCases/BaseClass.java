6fae2401e71a596bc4e3adbbfb685c25a8dc9524
Ashutosh Verma <ashutosh.verma1@timesinternet.in>
2020-05-22 00:06:30 +0530

    minor changes

--- src/test/java/com/automation/testCases/BaseClass.java
+++ src/test/java/com/automation/testCases/BaseClass.java
@@ -1,27 +1,18 @@
 package com.automation.testCases;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 import com.automation.utilities.ReadConfig;
